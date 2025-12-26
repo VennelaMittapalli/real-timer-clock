a60184a51cf4d3258329748d0d1ebd4648897e86
VennelaMittapalli <59090497+VennelaMittapalli@users.noreply.github.com>
2025-12-26 05:49:19 -0800

    Update real_timer_top.v

--- real_timer_top.v
+++ real_timer_top.v
@@ -5,8 +5,9 @@ module real_timer_top(input RST, CLK, output reg [6:0] HRM, HRL, MIN_M, MIN_L, S
 	real_timer realt(RST,CLK,HRM1,HRL1,MIN_M1,MIN_L1,SEC_M1,SEC_L1);
 	seven_segment sseg1(HRM1,HRM);
 	seven_segment sseg2(HRL1,HRL);
-	seven_segemnt sseg3(MIN_M1,MIN_M);
+	seven_segment sseg3(MIN_M1,MIN_M);
 	seven_segment sseg4(MIN_L1,MIN_L);
 	seven_segment sseg5(SEC_M1,SEC_M);
-	seven_segment sseg6(SEC)L1,SEC_L);
+	seven_segment sseg6(SEC_L1,SEC_L);
+
 endmodule
